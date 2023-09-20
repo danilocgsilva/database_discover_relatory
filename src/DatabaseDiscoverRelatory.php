1c9eee04917b77b067a7f7e6ccc50c873fcfc133
Danilo silva <contact@danilocgsilva.me>
2023-09-20 02:54:05 +0000

    cliTables

--- src/DatabaseDiscoverRelatory.php
+++ src/DatabaseDiscoverRelatory.php
@@ -1,15 +1,52 @@
 <?php
 
-declare(strict_type=1);
+declare(strict_types=1);
+
+namespace Danilocgsilva\DatabaseDiscoverRelatory;
 
 use PDO;
+use Danilocgsilva\DatabaseDiscover\DatabaseDiscover;
 
 class DatabaseDiscoverRelatory
 {
-    public function __construct(private PDO $pdo) {}
+    private DatabaseDiscover $databaseDiscover;
 
-    public function getTables()
+    private int $tablesCount = 0;
+
+    private int $databaseSize = 0;
+
+    public function __construct(PDO $pdo) {
+        $this->databaseDiscover = new DatabaseDiscover($pdo);
+    }
+
+    public function cliTables(): void
     {
+        foreach ($this->databaseDiscover->getTables() as $table) {
+            $this->tablesCount++;
+            print(sprintf(
+                "%s, %s\n",
+                ($tableName = $table->getName()),
+                $this->formatTableSize($tableName)
+            ));
+        }
 
+        print(sprintf("\nThe total count of tables is %s.\n", $this->tablesCount));
+        print(sprintf(
+            "The size of database is %s.\n", 
+            $this->formatSizeFromBytes($this->databaseSize)
+        ));
+    }
+
+    private function formatTableSize(string $tableName): string
+    {
+        $bytes = $this->databaseDiscover->getTableSize($tableName);
+        return $this->formatSizeFromBytes($bytes);
+    }
+
+    private function formatSizeFromBytes(int $bytes): string
+    {
+        $this->databaseSize += $bytes;
+        $tableSizeMb = $bytes / 1024 / 1024;
+        return number_format($tableSizeMb, 2, ",", ".") . " Mb";
     }
 }
