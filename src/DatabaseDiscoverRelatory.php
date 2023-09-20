aef5eb86c90f46e7f539fe83ee9436a867937557
Danilo Silva <contact@danilocgsilva.me>
2023-09-19 22:01:05 -0300

    First commit

---
+++ src/DatabaseDiscoverRelatory.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_type=1);
+
+use PDO;
+
+class DatabaseDiscoverRelatory
+{
+    public function __construct(private PDO $pdo) {}
+
+    public function getTables()
+    {
+
+    }
+}
